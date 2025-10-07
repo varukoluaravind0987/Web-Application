a8155acaf00b9e45b7d53a6ba177e1ee37ea936b
varukoluaravind22 <varukoluaravind22@gmail.com>
2025-10-07 15:09:51 +0000

    web server deployment script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+!#/bin/bash/
+
+echo "updating the server"
+sudo apt update -y
+
+echo "installing the utilities"
+sudo spt install zip unzip -y
+
+echo "creating webserver"
+sudo apt install nginx -y
+
+echo "Cleanup the directory"
+sudo rm -rf /var/www/html/
+
+echo "cloning the repository"
+sudo gir clone https://github.com/varukoluaravind0987/Web-Application.git /var/www/html/
+
+echo "Web server successfully deployed"
